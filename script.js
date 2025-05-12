6af981fa0fdcc533ad6e9a30655053833609c847
Manish <39481751+Manish123Sharma@users.noreply.github.com>
2025-05-12 22:33:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function toCase(text) {
   // write your code here
+	let lowerC = text.toLowerCase();
+	let upperC = text.toUpperCase();
+	
+	return `${lowerC}-${upperC}`;
 }
 
 // DO not change the code below
